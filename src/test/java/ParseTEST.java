83714ade2e7efbb6ae853764aef59068dabcf010
Marianne Adams <meadams2@bsu.edu>
2026-02-17 14:38:37 -0500

    First test case for Parser class

--- src/test/java/ParseTEST.java
+++ src/test/java/ParseTEST.java
@@ -1,4 +1,13 @@
 package PACKAGE_NAME;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class ParseTEST {
+    @Test
+    public void testParseInstructionSpecifier(){
+        Parser parser = new Parser();
+        String assemblyCode = "LDBA 0x0048, i";
+        String instructionSpecifier = parser.parseInstructionSpecifier();
+        Assertions.assertEquals("LDBA", instructionSpecifier);
+    }
 }
