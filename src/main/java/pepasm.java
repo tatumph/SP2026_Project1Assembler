585b24a934e05b1f224d4fcba3e5ebf30c054fcd
Tatum Hadley <0tatumhadley0@gmail.com>
2026-02-23 12:49:34 -0500

    Made an arraylist of strings inside readfile class

--- src/main/java/pepasm.java
+++ src/main/java/pepasm.java
@@ -1,7 +1,12 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class pepasm {
+    public List<String> charSet = new ArrayList<String>();
+
     public static String getFileName() {
         return "program1.pep";
     }
@@ -9,9 +14,11 @@ public class pepasm {
 
     public void readFile(String s) throws FileNotFoundException {
         File file = new File("code/" + s);
+        System.out.println("Original Assembly code: ");
         try (Scanner reader = new Scanner(file)) {
             while (reader.hasNextLine()) {
                 String line = reader.nextLine();
+                charSet.add(Arrays.toString(line.split(" ")));
                 System.out.println(line);
             }
         }
