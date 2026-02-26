087bdb24e6ac01808f02ff9345064072c491e157
Marianne Adams <meadams2@bsu.edu>
2026-02-25 20:04:47 -0500

    Well it works in command line (all we really care about). Will give more in detail explanation in morning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -13,6 +14,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("Pepasm")
 }
 
 tasks.test {
